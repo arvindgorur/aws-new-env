5225d409bbfb7d4144b342eaa079bdcf3079994b
Arvind Gorur <arvind.gorur@hotmail.com>
2018-05-29 15:54:35 -0400

    Updated main variables file

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "default_region" {
-  default = "us-east-1"
+  description = "Default region"
+  default     = "us-east-1"
 }
 
 variable "default_instance_type" {
-  default = "t2.micro"
+  description = "Default instance size"
+  default     = "t2.micro"
 }
 
 variable "default_vpc_security_group" {
-  default = "sg-9e51dbe9"
+  description = "Default VPC security group ID"
+  default     = "sg-9e51dbe9"
 }
 
 variable "default_security_group" {
-  default = "sg-60b40a29"
+  description = "Default custom security group ID"
+  default     = "sg-60b40a29"
 }
